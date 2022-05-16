d070e003a0e20efbbdece424e472981cf9460ee9
DinnerBeef <DinnerBeef18@gmail.com>
2022-05-16 11:23:41 -0500

    Added block hardness
    Block now need tools to break

--- src/main/java/me/dinnerbeef/compressium/CompressibleType.java
+++ src/main/java/me/dinnerbeef/compressium/CompressibleType.java
@@ -1,10 +1,7 @@
 package me.dinnerbeef.compressium;
 
 import com.google.gson.*;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.NetherrackBlock;
-import net.minecraft.world.level.block.PoweredBlock;
-import net.minecraft.world.level.block.SoulSandBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import org.apache.logging.log4j.LogManager;
@@ -17,11 +14,17 @@ import java.util.Optional;
 import java.util.function.Supplier;
 
 public enum CompressibleType {
-    BLOCK("block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE))),
-    FALLING("falling", () -> new FallingDamageBlock(BlockBehaviour.Properties.of(Material.SAND))),
-    NETHER_RACK("nether_rack", () -> new NetherrackBlock(BlockBehaviour.Properties.of(Material.STONE))),
-    SOUL_SAND("soul_sand", () -> new SoulSandBlock(BlockBehaviour.Properties.of(Material.SAND))),
-    POWERED("powered", () -> new PoweredBlock(BlockBehaviour.Properties.of(Material.STONE)));
+    BLOCK("block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(1.5f).requiresCorrectToolForDrops().sound(SoundType.STONE))),
+    METAL("block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(1.5f).requiresCorrectToolForDrops().sound(SoundType.METAL))),
+    NETHER_METAL("block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(1.5f).requiresCorrectToolForDrops().sound(SoundType.NETHERITE_BLOCK))),
+    DIRT("block", () -> new FallingDamageBlock(BlockBehaviour.Properties.of(Material.DIRT).strength(1.5f).sound(SoundType.GRAVEL))),
+    CLAY("block", () -> new FallingDamageBlock(BlockBehaviour.Properties.of(Material.CLAY).strength(1.5f).sound(SoundType.GRAVEL))),
+    COPPER("block", () -> new FallingDamageBlock(BlockBehaviour.Properties.of(Material.METAL).strength(1.5f).requiresCorrectToolForDrops().sound(SoundType.COPPER))),
+    SAND("falling", () -> new FallingDamageBlock(BlockBehaviour.Properties.of(Material.SAND).strength(1.5f).sound(SoundType.SAND))),
+    GRAVEL("falling", () -> new FallingDamageBlock(BlockBehaviour.Properties.of(Material.SAND).strength(1.5f).sound(SoundType.GRAVEL))),
+    NETHER_RACK("nether_rack", () -> new NetherrackBlock(BlockBehaviour.Properties.of(Material.STONE).strength(1.5f).requiresCorrectToolForDrops().sound(SoundType.NETHERRACK))),
+    SOUL_SAND("soul_sand", () -> new SoulSandBlock(BlockBehaviour.Properties.of(Material.SAND).strength(1.5f).sound(SoundType.SOUL_SAND))),
+    POWERED("powered", () -> new PoweredBlock(BlockBehaviour.Properties.of(Material.STONE).strength(1.5f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
 
     private String blockType;
     private Supplier<? extends Block> constructor;
--- src/main/java/me/dinnerbeef/compressium/DefaultCompressiumBlocks.java
+++ src/main/java/me/dinnerbeef/compressium/DefaultCompressiumBlocks.java
@@ -8,20 +8,20 @@ import java.util.List;
 public enum DefaultCompressiumBlocks {
     COBBLESTONE("cobblestone", "cobblestone", "block/cobblestone", "block/cobblestone", false),
     STONE("stone", "stone", "block/stone", "block/stone", false),
-    SAND("sand", "sand", "block/sand", "block/sand", CompressibleType.FALLING, false),
-    GRAVEL("gravel", "gravel", "block/gravel", "block/gravel",  CompressibleType.FALLING, false),
+    SAND("sand", "sand", "block/sand", "block/sand", CompressibleType.SAND, false),
+    GRAVEL("gravel", "gravel", "block/gravel", "block/gravel",  CompressibleType.GRAVEL, false),
     NETHERRACK("netherrack", "netherrack", "block/netherrack", "block/netherrack", CompressibleType.NETHER_RACK, false),
-    SNOW("snow", "snow_block", "block/snow", "block/snow_block", true),
+    SNOW("snow", "snow_block", "block/snow", "block/snow_block", CompressibleType.POWERED, true),
     SOULSAND("soulsand", "soul_sand", "block/soul_sand", "block/soul_sand", CompressibleType.SOUL_SAND, false),
-    IRON("iron", "iron_block", "block/iron_block", "block/iron_block"),
-    GOLD("gold", "gold_block", "block/gold_block", "block/gold_block"),
-    DIAMOND("diamond", "diamond_block", "block/diamond_block", "block/diamond_block"),
-    EMERALD("emerald", "emerald_block", "block/emerald_block", "block/emerald_block"),
-    CLAY("clay", "clay", "block/clay", "block/clay", false),
-    NETHERITE("netherite", "netherite_block", "block/netherite_block", "block/netherite_block"),
-    DIRT("dirt", "dirt", "block/dirt", "block/dirt"),
+    IRON("iron", "iron_block", "block/iron_block", "block/iron_block", CompressibleType.METAL, false),
+    GOLD("gold", "gold_block", "block/gold_block", "block/gold_block", CompressibleType.METAL, false),
+    DIAMOND("diamond", "diamond_block", "block/diamond_block", "block/diamond_block", CompressibleType.METAL, false),
+    EMERALD("emerald", "emerald_block", "block/emerald_block", "block/emerald_block", CompressibleType.METAL, false),
+    CLAY("clay", "clay", "block/clay", "block/clay", CompressibleType.CLAY, false),
+    NETHERITE("netherite", "netherite_block", "block/netherite_block", "block/netherite_block", CompressibleType.NETHER_METAL, false),
+    DIRT("dirt", "dirt", "block/dirt", "block/dirt", CompressibleType.DIRT, false),
     COAL("coal", "coal_block", "block/coal_block", "block/coal_block"),
-    REDSAND("redsand", "red_sand", "block/red_sand", "block/red_sand", CompressibleType.FALLING, true),
+    REDSAND("redsand", "red_sand", "block/red_sand", "block/red_sand", CompressibleType.SAND, true),
     ENDSTONE("endstone", "end_stone", "block/end_stone", "block/end_stone"),
     OBSIDIAN("obsidian", "obsidian", "block/obsidian", "block/obsidian"),
     LAPIS("lapis", "lapis_block", "block/lapis_block", "block/lapis_block"),
@@ -29,15 +29,14 @@ public enum DefaultCompressiumBlocks {
     REDSTONE("redstone", "redstone_block", "block/redstone_block", "block/redstone_block", CompressibleType.POWERED, true),
     ANDESITE("andesite", "andesite", "block/andesite", "block/andesite"),
     DIORITE("diorite", "diorite", "block/diorite", "block/diorite"),
-    COPPER("copper", "copper_block", "block/copper_block", "block/copper_block"),
+    COPPER("copper", "copper_block", "block/copper_block", "block/copper_block", CompressibleType.COPPER, false),
     GRANITE("granite", "granite", "block/granite", "block/granite");
 
     public static final List<DefaultCompressiumBlocks> VALUES = Arrays.asList(values());
 
     CompressibleBlock block;
 
-    DefaultCompressiumBlocks(String n, String baseResourceLocation, String particlePath, String baseBlockModel, boolean isBlockOf) {
-        this(n, baseResourceLocation, particlePath, baseBlockModel, CompressibleType.BLOCK, isBlockOf);
+    DefaultCompressiumBlocks(String n, String baseResourceLocation, String particlePath, String baseBlockModel, boolean isBlockOf) {        this(n, baseResourceLocation, particlePath, baseBlockModel, CompressibleType.BLOCK, isBlockOf);
     }
 
     DefaultCompressiumBlocks(String n, String baseResourceLocation, String particlePath, String baseBlockModel) {
